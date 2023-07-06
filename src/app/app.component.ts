3e78cbc8d602be0139bfe3090afdfd3fe7b8501b
stopcallingmyname <56963796+stopcallingmyname@users.noreply.github.com>
2023-07-06 13:30:53 +0300

    bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Modsen-Weather-Task';
+  title = 'Modsen Weather Task';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { GlobalErrorComponent } from './widgets/global-error/global-error.component';
-import { WeatherWidgetComponent } from './widgets/weather-widget/weather-widget.component';
-import { TimeDateWidgetComponent } from './widgets/time-date-widget/time-date-widget.component';
-import { NavbarComponent } from './widgets/navbar/navbar.component';
 import { CoreModule } from './modules/core/core.module';
 import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    GlobalErrorComponent,
-    WeatherWidgetComponent,
-    TimeDateWidgetComponent,
-    NavbarComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     CoreModule.forRoot(),
     SharedModule,
